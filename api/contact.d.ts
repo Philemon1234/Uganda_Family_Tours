44a5df7ff905b40e701449a6e17d287818924494
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2026-06-29 19:49:50 +0300

    Remove ignored files from tracking

--- api/contact.d.ts
+++
@@ -1,7 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http'
-
-type ContactRequest = IncomingMessage & {
-  body?: unknown
-}
-
-export default function handler(request: ContactRequest, response: ServerResponse): Promise<void>
