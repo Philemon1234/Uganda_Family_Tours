d9dd152bd284d823342b71f70babf1395e59b8c8
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2026-07-04 12:50:35 +0300

    Added image

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,13 +1,13 @@
-import safariTruck from '../assets/Venture-Uganda-Safari-Uganda-01.jpg'
 import lake from '../assets/3.jpeg'
 import culture from '../assets/4.jpeg'
 import forest from '../assets/2D3A1324-scaled.webp'
+import centerGorilla from '../assets/Africa-Gorilla-GettyImages-986556120.jpg'
 import gorilla from '../assets/gorilla-7708328_1920.jpg'
 import lions from '../assets/cover_1669-Tree-Climbing-Lions.jpg'
 
 export const galleryImages = [
   { src: lake, altKey: 'gallery.images.lake' },
-  { src: safariTruck, altKey: 'gallery.images.safariTruck' },
+  { src: centerGorilla, altKey: 'gallery.images.gorilla' },
   { src: culture, altKey: 'gallery.images.culture' },
   { src: forest, altKey: 'gallery.images.forest' },
   { src: gorilla, altKey: 'gallery.images.gorilla' },
